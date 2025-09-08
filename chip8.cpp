b49d286bbd3c71986261490bfb2c9c08a19606d9
Pedro-Pereira0 <pedrodanielmilheirosilvapereira@outlook.pt>
2025-09-08 16:28:43 +0100

    Finished the methods initialize and loadGame

--- chip8.cpp
+++ chip8.cpp
@@ -1,4 +1,5 @@
 #include "chip8.h"
+
 class chip8{
     //chip-8 system
     //35 operation codes, 2bytes long 
@@ -29,6 +30,28 @@ class chip8{
 
     //keypad
     unsigned char key[16];
+
+    unsigned char chip8_fontset[80] =
+    { 
+        0xF0, 0x90, 0x90, 0x90, 0xF0, //0
+        0x20, 0x60, 0x20, 0x20, 0x70, //1
+        0xF0, 0x10, 0xF0, 0x80, 0xF0, //2
+        0xF0, 0x10, 0xF0, 0x10, 0xF0, //3
+        0x90, 0x90, 0xF0, 0x10, 0x10, //4
+        0xF0, 0x80, 0xF0, 0x10, 0xF0, //5
+        0xF0, 0x80, 0xF0, 0x90, 0xF0, //6
+        0xF0, 0x10, 0x20, 0x40, 0x40, //7
+        0xF0, 0x90, 0xF0, 0x90, 0xF0, //8
+        0xF0, 0x90, 0xF0, 0x10, 0xF0, //9
+        0xF0, 0x90, 0xF0, 0x90, 0x90, //A
+        0xE0, 0x90, 0xE0, 0x90, 0xE0, //B
+        0xF0, 0x80, 0x80, 0x80, 0xF0, //C
+        0xE0, 0x90, 0x90, 0x90, 0xE0, //D
+        0xF0, 0x80, 0xF0, 0x80, 0xF0, //E
+        0xF0, 0x80, 0xF0, 0x80, 0x80  //F
+    };
+
+
     
     public:
         bool drawFlag;
@@ -39,20 +62,77 @@ class chip8{
             opcode = 0;      // Reset current opcode	
             I      = 0;      // Reset index register
             sp     = 0;      // Reset stack pointer
-            // Clear display	
+            
+            // Clear display
+            for(int i = 0; i < 2048; ++i)
+                gfx[i] = 0;
+
             // Clear stack
-            // Clear registers V0-VF
+            for(int i = 0; i < 16; ++i)
+                stack[i] = 0;
+
+            for(int i = 0; i < 16; ++i)
+                key[i] = V[i] = 0;
+
             // Clear memory
+            for(int i = 0; i < 4096; ++i)
+                memory[i] = 0;
 
             // Load fontset
             for(int i = 0; i < 80; ++i){
-                //memory[i] = chip8_fontset[i];		
+                memory[i] = chip8_fontset[i];		
             }
             // Reset timers
+            delay_timer = 0;
+            sound_timer = 0;
         }
 
-        void loadGame(std::string gameName){
+        bool loadGame(char* gameName){
 
+            printf("Loading %s\n", gameName);
+            //Opening the application as binary
+            FILE* fp = fopen(gameName, "rb");
+            if(fp == NULL)
+            {
+                fputs("File error", stderr);
+                return false;
+            }
+
+            //Check file size
+            fseek(fp , 0 , SEEK_END);
+            long lSize = ftell(fp);
+            rewind(fp);
+            printf("Filesize: %d\n", (int)lSize);
+
+            //Allocate the memory to contain the whole file
+            char* buffer = (char*)malloc(sizeof(char) * lSize);
+            if (buffer == NULL){
+                fputs("Memory error", stderr);
+                return false;
+            }
+            
+            //Copy the file into the buffer
+            size_t result = fread(buffer, 1, lSize, fp);
+            if (result != lSize) 
+            {
+                fputs("Reading error",stderr); 
+                return false;
+            }
+
+            //Copy buffer to Chip8 memory
+            if((4096-512) > lSize)
+            {
+                for(int i = 0; i < lSize; ++i)
+                    memory[i + 512] = buffer[i];
+            }
+            else
+                printf("Error: ROM too big for memory");
+            
+            // Close file, free buffer
+            fclose(fp);
+            free(buffer);
+
+            return true;
         }
         void setKeys(){
 
